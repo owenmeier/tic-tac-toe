23d93ee5a417e991dac87cb92e012371f3a7ab92
Owen Meier <owen2717@gmail.com>
2025-06-18 14:10:34 -0500

    created grid, handleclick function inside square component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
-export default function Square() {
-  return <button className="square">X</button>;
+export default function Board() {
+  function Square({ value }) {
+    function handleClick() {
+      console.log(`Square ${value} clicked`);
+    }
+
+    return (
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+    );
+  }
+
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }
