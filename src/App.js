22f0691e3d7dd8737e5edd73a5419b69b43a2813
Owen Meier <owen2717@gmail.com>
2025-06-22 20:08:39 -0500

    got game working again with game function as default export with board nested within

--- src/App.js
+++ src/App.js
@@ -8,18 +8,12 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board(xIsNext, squares, onPlay) {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(index) {
     if (squares[index] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
-    if (nextSquares[index]) {
-      return;
-    }
     if (xIsNext) {
       nextSquares[index] = "X";
     } else {
@@ -34,30 +28,6 @@ function Board(xIsNext, squares, onPlay) {
     status = "Winner: " + winner;
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
   }
 
   return (
@@ -82,15 +52,34 @@ function Board(xIsNext, squares, onPlay) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState(Array(9).fill(null));
-  currentSquares = history[history.length - 1];
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
 
   function handlePlay(squares) {
-    {
-      /*ToDO*/
-    }
+    setHistory([...history, squares]);
+    setXIsNext(!xIsNext);
   }
 
   return (
