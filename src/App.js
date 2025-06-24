1bdb0c1d8ee2598d2d8c9821a1c61a28c40a4bc4
Owen Meier <owen2717@gmail.com>
2025-06-23 21:48:50 -0500

    commits are current

--- src/App.js
+++ src/App.js
@@ -113,18 +113,27 @@ export default function Game() {
           />
         </div>
       </div>
-      <div className="game-info">
-        <input
-          type="range"
-          min="0"
-          max={history.length - 1}
-          value={currentMove}
-          onChange={(e) => jumpTo(Number(e.target.value))}
-        ></input>
-      </div>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <input
+        type="range"
+        min="0"
+        max={history.length - 1}
+        value={currentMove}
+        onChange={(e) => jumpTo(Number(e.target.value))}
+        style={{ width: "100px", display: "block" }}
+      ></input>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100px",
+          fontSize: "0.8em",
+          marginTop: "4px",
+        }}
+      >
         {Array.from({ length: history.length }, (_, i) => (
-          <span key={i}>{i}</span>
+          <span key={i} style={{ textAlign: "center", width: "1.5em" }}>
+            {i}
+          </span>
         ))}
       </div>
     </>
